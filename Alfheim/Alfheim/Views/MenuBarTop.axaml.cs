46047bcc8b05d44e2e80f013e5463a267150ac84
KMT-0 <kuba.prochazka@gmail.com>
2022-11-15 18:51:54 +0100

    #2

---
+++ Alfheim/Alfheim/Views/MenuBarTop.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Alfheim.Views;
+
+public partial class MenuBarTop : UserControl
+{
+    public MenuBarTop()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
